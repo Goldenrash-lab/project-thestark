6a4f588f5b129cdf7c3321f9ee1d30f36c03b8e6
Yehor Hryhortsev <egor.goldenrash@gmail.com>
2023-10-06 12:52:50 +0300

    add import jquery

--- src/main.js
+++ src/main.js
@@ -1,21 +1,21 @@
-// import $ from "jquery"
-// import "slick-carousel"
+import $ from "jquery"
+import "slick-carousel"
 
 
-// $(".review-list").slick({
-//     arrows: false,
-//     infinite: true,
-//     speed: 300,
-//     slidesToShow: 1,
-//     autoplay: true,
-//     // fade: true,
-//     // cssEase: 'linear',
-//     centerMode: false,
-//     mobileFirst: true,
-//   responsive: [
-//       { 
-//           breakpoint: 1279,
-//           settings: "unslick",
-//     }
-// ]
-// });
+$(".review-list").slick({
+    arrows: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    autoplay: true,
+    // fade: true,
+    // cssEase: 'linear',
+    centerMode: false,
+    mobileFirst: true,
+  responsive: [
+      { 
+          breakpoint: 1279,
+          settings: "unslick",
+    }
+]
+});
